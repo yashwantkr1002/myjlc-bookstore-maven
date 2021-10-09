b97b619eb50e9dd536a681116afc5606695ee045
yashwantkr1002 <89151396+yashwantkr1002@users.noreply.github.com>
2021-10-09 09:09:11 +0530

    Update - by Yashwant

--- src/main/java/com/jlcindia/springboot/MyBootApplication.java
+++ src/main/java/com/jlcindia/springboot/MyBootApplication.java
@@ -9,6 +9,7 @@ public static void main(String[] args) {
 System.out.println("Starting Boot Application");
 System.out.println("Update -1 Srinivas");
   System.out.println("Update -2 by Srinivas");
+  System.out.println("Update -1 Yashwant");
 
 SpringApplication.run(MyBootApplication.class, args);
 }
